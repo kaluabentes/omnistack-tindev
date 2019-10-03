8dc7f93fcefebd933fcfbc454c97fefef42390ba
Kaluã Bentes <kaluanbentes@gmail.com>
2019-10-02 22:10:54 -0300

    Add mongoose

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const routes = require("./routes");
 
 const server = express();
 
+mongoose.connect(
+  "mongodb+srv://kalu:kalu@cluster0-fgh3x.mongodb.net/admin?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+server.use(express.json());
 server.use(routes);
 
 server.listen(3333);
