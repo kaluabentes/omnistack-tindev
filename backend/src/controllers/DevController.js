be844885d25e5a85a485c1b2fc920fcf4deefa55
Kaluã Bentes <kaluanbentes@gmail.com>
2019-10-02 23:08:08 -0300

    Create index method

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -4,6 +4,22 @@ const Dev = require("../models/Dev");
 const GITHUB_API_URL = "https://api.github.com/users/";
 
 module.exports = {
+  async index(req, res) {
+    const { user } = req.headers;
+
+    const loggedDev = await Dev.findById(user);
+
+    const users = await Dev.find({
+      $and: [
+        { _id: { $ne: user } },
+        { _id: { $nin: loggedDev.likes } },
+        { _id: { $nin: loggedDev.dislikes } }
+      ]
+    });
+
+    return res.json(users);
+  },
+
   async store(req, res) {
     const { username } = req.body;
 
